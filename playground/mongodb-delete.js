bfefdefc297c29dc395b38da84e30939c058d778
Asif Hussain <asifhussain331@gmail.com>
2018-12-21 13:13:33 +0500

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,34 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp')
+
+  // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
+
+  // deleteOne
+  // db.collection('Todos').deleteOne({text: 'Eat Lunch'}).then((result) => {
+  //   console.log(result);
+  // });
+
+  // findOneAndDelete
+  // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+  //   console.log(result);
+  // });
+
+  // db.collection('Users').deleteMany({name: 'Asif'});
+
+  db.collection('Users').findOneAndDelete({
+    _id: new ObjectID("5c1c9ef85f332cb606ce58e3")
+  }).then((results) => {
+    console.log(JSON.stringify(results, undefined, 2));
+  });
+
+  // db.close();
+});
