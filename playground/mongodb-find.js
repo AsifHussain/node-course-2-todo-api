ac6aac93db95bfe295d1344255efbd1ba58d961e
Asif Hussain <asifhussain331@gmail.com>
2018-12-22 12:08:50 +0500

    Add POST /todos route and refactor mongoose

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -8,14 +8,14 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true
   console.log('Connected to MongoDB server');
   const db = client.db('TodoApp')
 
-  db.collection('Todos').find({
-    //_id: new ObjectID('57bb36afb3b6a3801d8c479d')
-  }).toArray().then((docs) => {
-    console.log('Todos');
-    console.log(JSON.stringify(docs, undefined, 2));
-  }, (err) => {
-    console.log('Unable to fetch todos', err);
-  });
+  // db.collection('Todos').find({
+  //   //_id: new ObjectID('57bb36afb3b6a3801d8c479d')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
 
   // db.collection('Todos').find().count().then((count) => {
   //   console.log(`Todos count: ${count}`);
@@ -23,9 +23,9 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true
   //   console.log('Unable to fetch todos', err);
   // });
 
-  // db.collection('Users').find({name: 'Andrew'}).toArray().then((docs) => {
-  //   console.log(JSON.stringify(docs, undefined, 2));
-  // });
+  db.collection('Users').find({name: 'Andrew'}).toArray().then((docs) => {
+    console.log(JSON.stringify(docs, undefined, 2));
+  });
 
   // db.close();
 });
